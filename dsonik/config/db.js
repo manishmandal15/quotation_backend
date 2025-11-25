7bc4b8f85b1a31d447b6c9c0ef91ace67a96a46a
dharmendra <dharmendra@cdac.in>
2025-11-25 22:27:16 +0530

    dyanamic menu as per user

--- dsonik/config/db.js
+++ dsonik/config/db.js
@@ -2,21 +2,48 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,       // Max connections
+//   queueLimit: 0              // Unlimited queue
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MySQL:", err);
+//     return;
+//   }
+//   console.log("Connected to MySQL database");
+// });
+
+
+// module.exports = db;
+
+// const mysql = require("mysql2");
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3309
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,       // Max connections
+  queueLimit: 0              // Unlimited queue
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
+    console.error("❌ DB Connection Failed:", err.code);
+  } else {
+    console.log("✅ MySQL Connected");
+    connection.release();
   }
-  console.log("Connected to MySQL database");
 });
 
-module.exports = db;
-
+module.exports = pool;
