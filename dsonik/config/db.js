1efd85638184e92a1fe0ddaca2d0b8cef5301310
dharmendra <dharmendra@cdac.in>
2025-11-25 22:36:20 +0530

    dyanamic menu as per user

--- dsonik/config/db.js
+++ dsonik/config/db.js
@@ -2,34 +2,10 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 3306,
-//   waitForConnections: true,
-//   connectionLimit: 10,       // Max connections
-//   queueLimit: 0              // Unlimited queue
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to MySQL:", err);
-//     return;
-//   }
-//   console.log("Connected to MySQL database");
-// });
-
-
-// module.exports = db;
-
-// const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
@@ -37,13 +13,37 @@ const pool = mysql.createPool({
   queueLimit: 0              // Unlimited queue
 });
 
-pool.getConnection((err, connection) => {
+db.connect((err) => {
   if (err) {
-    console.error("❌ DB Connection Failed:", err.code);
-  } else {
-    console.log("✅ MySQL Connected");
-    connection.release();
+    console.error("Error connecting to MySQL:", err);
+    return;
   }
+  console.log("Connected to MySQL database");
 });
 
-module.exports = pool;
+
+module.exports = db;
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3309,
+//   waitForConnections: true,
+//   connectionLimit: 10,       // Max connections
+//   queueLimit: 0              // Unlimited queue
+// });
+
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.error("❌ DB Connection Failed:", err.code);
+//   } else {
+//     console.log("✅ MySQL Connected");
+//     connection.release();
+//   }
+// });
+
+// module.exports = pool;
